87ca68ca2954e14324e5cbc7e1a48d2b793e75e7
Fc Guerrero Diaz <ginetagris@gmail.com>
2022-03-04 14:33:29 +0100

    fin seccion 4

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// modulo
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+const routes: Routes = [
+
+
+  // path: ./pages/pages.routing  paginas
+  // path: ./auth/auth.routing    auth
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent },
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
